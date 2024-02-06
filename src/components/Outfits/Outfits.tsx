85f6142258bf1e0ea75c70336daec9ad992076a1
haagen24 <40901301+haagen24@users.noreply.github.com>
2024-02-06 15:06:01 -0500

    fix fav page icon issue

--- src/components/Outfits/Outfits.tsx
+++ src/components/Outfits/Outfits.tsx
@@ -57,32 +57,33 @@ const Outfits: React.FC<OutfitsProps> = ({
         getClothings();
     }, [outfits]);
 
-    // useEffect(() => {
-    //     const fetchFavorites = async () => {
-    //         if (token) {
-    //             try {
-    //                 const response = await axios.get(
-    //                     fixUrl(API_URL, `api/profile/favorite`),
-    //                     {
-    //                         headers: {
-    //                             Authorization: `Bearer ${token}`,
-    //                         },
-    //                     }
-    //                 );
-    //                 const favoriteIds = response.data.map(
-    //                     (fav: { id: number; outfit_pic_link: string }) => ({
-    //                         outfit_id: fav.id,
-    //                     })
-    //                 );
-    //                 setFavorites(favoriteIds);
-    //             } catch (error) {
-    //                 console.error("Failed to fetch favorites", error);
-    //             }
-    //         }
-    //     };
+    useEffect(() => {
+        const fetchFavorites = async () => {
+            if (token) {
+                try {
+                    const response = await axios.get(
+                        fixUrl(API_URL, `api/profile/favorite`),
+                        {
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    const favoriteIds = response.data.map(
+                        (fav: { id: number; outfit_pic_link: string }) => ({
+                            outfit_id: fav.id,
+                        })
+                    );
+                    setFavorites(favoriteIds);
+                    console.log(response);
+                } catch (error) {
+                    console.error("Failed to fetch favorites", error);
+                }
+            }
+        };
 
-    //     fetchFavorites();
-    // }, [token]);
+        fetchFavorites();
+    }, [token]);
 
     const handleOutfitShow = (outfitId: number) => {
         setSelectedOutfit(outfitId);
@@ -156,24 +157,7 @@ const Outfits: React.FC<OutfitsProps> = ({
                                     />
                                     {selectedOutfit === outfit.id ? (
                                         <div className="outfits__details">
-                                            {currentUser && outfit.id ? (
-                                                <svg
-                                                    onClick={() => {
-                                                        if (handleDelete) {
-                                                            handleDelete(
-                                                                outfit.id
-                                                            );
-                                                        }
-                                                    }}
-                                                    className="outfits__icon"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    height="40"
-                                                    viewBox="0 -960 960 960"
-                                                    width="40"
-                                                    fill="white">
-                                                    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-                                                </svg>
-                                            ) : token && outfit.id ? (
+                                            {token && outfit.id ? (
                                                 <svg
                                                     onClick={() => {
                                                         if (handleFav) {
